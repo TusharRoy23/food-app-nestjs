5ccbf8e268f7944cba10878eb22fe20e5d718b85
TusharRoy23 <risenboy23@gmail.com>
2023-03-04 13:29:44 +0600

    request service injected

--- src/modules/shared/service/request.service.ts
+++ src/modules/shared/service/request.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from "@nestjs/common";
 import { User } from "../../user/schemas/user.schema";
 import { IRequestService } from "../interfaces/IRequest.service";
 
+@Injectable()
 export class RequestService implements IRequestService {
     private user: User;
 
