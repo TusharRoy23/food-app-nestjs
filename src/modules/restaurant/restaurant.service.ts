a5bd76df1f407d162c09ecfdb9789ecd989c4bb4
TusharRoy23 <risenboy23@gmail.com>
2023-03-20 16:16:51 +0600

    elastic search modified

--- src/modules/restaurant/restaurant.service.ts
+++ src/modules/restaurant/restaurant.service.ts
@@ -207,7 +207,7 @@ export class RestaurantService implements IRestaurantService {
                 { restaurant: user.restaurant._id },
                 { start_date: { $lte: new Date(orderDiscountDto.start_date) } },
                 { end_date: { $gte: new Date(orderDiscountDto.end_date) } }
-            ]);
+            ]).exec();
 
             if (orderDis.length) {
                 throw new BadRequestException('Already exists.');
